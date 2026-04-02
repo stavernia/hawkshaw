83633b47d080395cb77ff3a5514e9a3d5a14d07d
Steven Tavernia <stavernia@truefiction.com>
2026-04-02 13:08:49 -0700

    feat: update Node and pnpm versions, add .nvmrc, and introduce type definitions for Next.js segment config

---
+++ next-compat.d.ts
@@ -0,0 +1,4 @@
+declare module "next/dist/build/segment-config/app/app-segment-config.js" {
+  export type PrefetchForTypeCheckInternal = unknown;
+  export type InstantConfigForTypeCheckInternal = unknown;
+}
